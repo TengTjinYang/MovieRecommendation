a0246b391a8119b7b18ac561163c20b2d1591bbc
TengTjinYang <101085998+TengTjinYang@users.noreply.github.com>
2023-10-17 14:58:54 +0800

    Update movie_recommendation.py
    Removed context input

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -2,21 +2,19 @@ import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # Initialize tokenizer and model
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium") # 'gpt2-medium' is one of the available GPT-2 models. You can also use 'gpt2', 'gpt2-large', or 'gpt2-xl'.
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium")
 model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
 model.eval()  # Set the model to evaluation mode
 
-def generate_response(context, user_query, max_length=100):
+def generate_response(user_query, max_length=100):
     """
     context: str, the top-k movie entries or descriptions retrieved.
     user_query: str, the original query from the user.
     max_length: int, maximum length of the generated response.
     """
-    # Combine the context and user query
-    input_text = context + user_query
     
     # Tokenize the input text
-    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    input_ids = tokenizer.encode(user_query, return_tensors="pt")
     
     # Generate a response from the model
     with torch.no_grad():
@@ -27,7 +25,6 @@ def generate_response(context, user_query, max_length=100):
 
     return generated_text
 
-context = "Movie 1: Inception - A mind-bending thriller by Christopher Nolan. Movie 2: Interstellar - A journey through space and time. "
-user_query = "Can you suggest a movie similar to Harry Potter released between 2000 and 2010 in the fantasy genre?"
-response = generate_response(context, user_query)
+user_query = "Can you suggest a movie released between 2000 and 2010 with Chris Hemsworth in it?"
+response = generate_response(user_query)
 print(response)
